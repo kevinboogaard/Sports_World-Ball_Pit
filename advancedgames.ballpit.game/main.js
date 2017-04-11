e205950b3e855271da0f7c72c40b8784578a15f3
Alex Antonides <15697@ma-web.nl>
2017-04-11 14:58:17 +0200

    (FEATURE) - 5-framework-inputsystem (#11)
    * (FEATURE) - 5-framework-inputsystem
    
    https://trello.com/c/lszxt5NU/67-developers-inputsystem
    
    Made some changes to the preloader/sceneloader. Added an input system and made the canvas work.
    
    * (FEATURE) - 5-framework-inputsystem
    
    https://trello.com/c/lszxt5NU/67-developers-inputsystem
    
    Added comments.

--- advancedgames.ballpit.game/main.js
+++ advancedgames.ballpit.game/main.js
@@ -1,2 +1,2 @@
 // If the webpage loads, call the ADCore framework's initialize function.
-window.onload = initialize; // We may refactor later on.
+window.onload = Initialize; // We may refactor later on.
