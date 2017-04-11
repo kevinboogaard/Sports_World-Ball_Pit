e6c8eb9e4f9f99c164f27bf34b9f788cebb0b16e
kevinboogaard <kevin.boogaard3@gmail.com>
2017-04-11 09:11:58 +0200

    FEATURE - 1-framework-preloader (#9)
    * FEATURE - 1-framework-preloader
    
    https://github.com/kevinboogaard/Sports_World-Ball_Pit/issues/2
    
    Installed Phaser Framework and made a preloader system.
    
    * FEATURE - 1-framework-preloader
    
    https://github.com/kevinboogaard/Sports_World-Ball_Pit/issues/2
    
    Updated commentary.

---
+++ advancedgames.ballpit.game/main.js
@@ -0,0 +1,2 @@
+// If the webpage loads, call the ADCore framework's initialize function.
+window.onload = initialize; // We may refactor later on.
---
+++ advancedgames.ballpit.settings/global.js
@@ -0,0 +1,6 @@
+var Global = {};
+
+Global.Loaded = {
+    "GENERIC": {},
+    "LEVEL": {}
+};
