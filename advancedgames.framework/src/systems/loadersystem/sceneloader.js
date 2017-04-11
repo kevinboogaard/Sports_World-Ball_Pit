de235fae11d4ccf00525ca25f37afe6cdb55c76b
kevinboogaard <kevin.boogaard3@gmail.com>
2017-04-11 12:33:18 +0200

    (FEATURE) - 3-framework-sceneloader (#10)
    https://trello.com/c/gASazn1K/66-developers-sceneloader
    
    The sceneloader can now load switch and dispatch scene's.

---
+++ advancedgames.framework/src/systems/loadersystem/sceneloader.js
@@ -0,0 +1,41 @@
+var ADCore = ADCore || {};
+
+ADCore.Sceneloader = (function () {
+
+     /**
+     * 'Sceneloader'
+     */
+    function Sceneloader() {
+        this.current = null;
+    }
+    var p = Sceneloader.prototype;
+    
+    /**
+     * 'load'
+     * @param {scene} 'scene'
+     * 
+     */
+    p.load = function(scene){
+        this.current = new scene();
+    };
+
+    /**
+     * 'switch'
+     * @param {scene} 'scene'
+     * 
+     */
+    p.switch = function(scene) {
+        this.disposeCurrent();
+        this.load(scene);
+    };
+
+    /**
+     * 'disposeCurrent'
+     */
+    p.disposeCurrent = function(){
+        this.current.dispose();
+        this.current = null;
+    };
+
+    return Sceneloader;
+}());
