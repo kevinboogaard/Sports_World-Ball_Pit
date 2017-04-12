545cefc1b6ffd2774774a922e64eac3010e66159
kevinboogaard <kevin.boogaard3@gmail.com>
2017-04-12 11:17:11 +0200

    (FEATURE)- 7-game-balls (#20)
    https://trello.com/b/QYvnYKT0/sports-world-ball-pit
    
    Preloader fixed, an ball model made.

--- advancedgames.framework/src/entities/entity.js
+++ advancedgames.framework/src/entities/entity.js
@@ -5,13 +5,16 @@ ADCore.Entity = (function () {
     /**
      * 'Entity'
      */
-    function Entity() {
-        this.position = new Vector2();
+    function Entity(position) {
+        this.position = position;
 
         this.disposed = false;
+
+        ADCore.EnableMutators( this );
+        this._gettersAndSetters();
     }
     var p = Entity.prototype;
-    
+
     /**
      * 'Dispose'
      *  with the dispose funtion you can clear all the data of an object and then destroy it.
@@ -20,6 +23,29 @@ ADCore.Entity = (function () {
         delete this.position;
 
         this.disposed = true;        
+    };
+
+    /**
+     * 'Getters And Setters'
+     */
+    p._gettersAndSetters = function () {
+        this.Define("x", {
+            "get": function () {
+                return this.position.x;
+            },
+            "set": function (value) {
+                this.position.x = value;
+            }
+        });
+
+        this.Define("y", {
+            "get": function () {
+                return this.position.y;
+            },
+            "set": function (value) {
+                this.position.y = value;
+            }
+        });
     };
 
     return Entity;
