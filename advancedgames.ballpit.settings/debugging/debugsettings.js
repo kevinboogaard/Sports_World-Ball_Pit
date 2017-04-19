a8a965e7a202dad7e8aaeb2f1c8447d8562cc74d
kevinboogaard <kevin.boogaard3@gmail.com>
2017-04-19 13:15:42 +0200

    (FEATURE) - 11-game-ball-prototype
    No Issue.
    Made the tutorial scene.

--- advancedgames.ballpit.settings/debugging/debugsettings.js
+++ advancedgames.ballpit.settings/debugging/debugsettings.js
@@ -1,8 +1,8 @@
 var Debug = new ADCore.Debug();
-Debug.ENABLED = true;
+Debug.ENABLED = false;
 
 Debug.FORCE_LOAD_DEBUG_LEVEL = false;
 Debug.DEBUG_LEVEL = 0;
 
 Debug.FORCE_LOAD_SCENE = false;
-Debug.DEBUG_SCENE = scene.Gridscene;
+Debug.DEBUG_SCENE = scene.Tutorialscene;
