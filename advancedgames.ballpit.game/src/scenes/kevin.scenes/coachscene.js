9137e0de83f9639f1e92b67c6b0dee68422a7d83
kevinboogaard <kevin.boogaard3@gmail.com>
2017-05-09 15:46:55 +0200

    (FEATURE) 24-game-task-handler
    https://github.com/kevinboogaard/Sports_World-Ball_Pit/issues/46
    Added the coach view in the game.

--- advancedgames.ballpit.game/src/scenes/kevin.scenes/coachscene.js
+++ advancedgames.ballpit.game/src/scenes/kevin.scenes/coachscene.js
@@ -7,7 +7,17 @@ scene.Coachscene = (function () {
      */
     function Coachscene() {
         Phaser.Group.call(this, ADCore.phaser, null, "Coachscene");
-        this.coach = new ballpit.CoachModel(Global.Loaded.level.tasks);
+        
+        var halfWidth = Config.Core.Dimensions.width / 2;
+
+       this.soccercoach = new ADCore.Interface(new Vector2(0, 0), "soccercoach");
+        this.soccercoach.anchor.set(0.5, 0.5);
+        this.soccercoach.x = halfWidth;
+        this.soccercoach.y = 100;
+        this.soccercoach.scale.setTo(0.5,0.5);
+        this.addChild(this.soccercoach);
+
+        this.soccercoach.Play("idle_super_angry",60,true);
     }
     Coachscene.prototype = Object.create(Phaser.Group.prototype);
     Coachscene.prototype.constructor = Coachscene; 
